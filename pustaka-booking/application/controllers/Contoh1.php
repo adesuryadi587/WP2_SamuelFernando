c83ac9c6276b738caac3db930a1f4ae431b99d78
samuel <samuelnandobing@gmail.com>
2022-03-30 11:14:28 +0700

    penambahan ada di readme

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Samuel Fernando
+              Saya tinggal di daerah Jakarta Utara
+              Olahraga yang saya sukai adalah
+              Futsal";
+    }
+}
---
+++ pustaka-booking/application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Samuel</li>
+        <li>Nama Belakang : Fernando</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan ciledug Raya No. 168 Pesanggarahan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Medan</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Badminton</li>
+    </ul>
+</section>
